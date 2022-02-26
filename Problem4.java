3f1aa7e9250ad31d6643368682339da16fc7b94a
f4ejkai <40180903+f4ejkai@users.noreply.github.com>
2022-02-26 00:53:26 -0800

    Update Problem4.java

--- Problem4.java
+++ Problem4.java
@@ -12,17 +12,39 @@ public class MultiplyMatrices {
         
         // Input your code here: 
         
+        //int product = 0;
+        int roll_1 = 2, column_1 = 3;
+        int roll_2 = 3, column_2 = 2;
+        int[][] product = new int [roll_1][column_2];
+        for (int i=0; i<roll_1;i++)
+        {
+            for(int j=0;j<column_2;j++)
+            {
+                for (int k=0; k<column_1;k++)
+                {
+                    product[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
+                }
+            }
+        }
 
-        displayProduct(product); 
-    }
-
-    public static void displayProduct(int[][] product) {
-        System.out.println("Product of two matrices is: ");
-        for(int[] row : product) {
-            for (int column : row) {
+        //displayProduct(product); 
+        System.out.println("the product of the two array is ");
+        for (int[] row : product) {
+            for (int column: row) {
                 System.out.print(column + "    ");
             }
             System.out.println();
         }
+
     }
+
+    //public static void displayProduct(int[][] product) {
+        //System.out.println("Product of two matrices is: ");
+        //for(int[] row : product) {
+            //for (int column : row) {
+                //System.out.print(column + "    ");
+            //}
+            //System.out.println();
+        //}
+    //}
 }
